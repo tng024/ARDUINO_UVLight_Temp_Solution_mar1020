ad5351e8a1ba0b226c51f1c177787a61cb7801b7
SINTEFGRP\thanh-tuann <thanhtuan.cn@gmail.com>
2020-03-10 09:20:19 +0100

    Initial commit

---
+++ ARDUINO_UVLight_Temp_Solution_mar1020.ino
@@ -0,0 +1,135 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}#include <ESP8266WiFi.h>        //I can connect to a Wifi
+#include <ESP8266WebServer.h>   //I can be a server 'cos I have the class ESP8266WebServer available
+#include <WiFiClient.h>
+
+#include "Relay.hpp"
+#include "PIR.hpp"
+
+const char *ssid = "ESPap";  
+const char *password = "yourpassword"; 
+ESP8266WebServer server(80);    
+
+Relay relay(12);
+PIR PIR_sensor(1,3,15,13,12,14);
+
+
+
+const int dirPin = 5; //This pin corresponds to GPIO5 (D1) (Yellow wire) https://nodemcu.readthedocs.io/en/latest/en/modules/gpio/
+const int stepPin = 4; //This pin corresponds to GPIO4 (D2) (Orange wire)
+int steps = 0; //This variable is related to the number of turns. If microstepping is disabled, 200 corresponds to a complete turn.
+int stepDelay = 0; //This variable is the pulse duration in milliseconds and it is related to the rotation speed. Without microstepping, 1.8º are stepDelay ms.
+bool dir = HIGH; //Rotation direction. HIGH is clockwise.
+
+
+void setup() {
+  pinMode(dirPin, OUTPUT);  // Pins are outputs
+  pinMode(stepPin, OUTPUT);
+
+  PIR_sensor.configuration();
+  PIR_sensor.pirCheck();
+
+  delay(1000);
+  Serial.begin(115200);           //I can debbug through the serial port
+
+  // Configure NODEMCU as Access Point
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid); //Password is not necessary
+  IPAddress myIP = WiFi.softAPIP(); //Get the IP assigned to itself.
+  Serial.print("AP IP address: "); //This is written in the PC console.
+  Serial.println(myIP);
+
+  server.on("/", handleRootPath); 
+
+  server.on("/Init", handleInit);   //Associate the handler function to the path "/Init".
+
+  server.on("/RelayControl", handleRelayControl);   //Associate the handler function to the path "/RelayControl".  
+  
+  server.on("/Timer", handleTimer);   //Associate the handler function to the path "/Timer".  
+  
+  server.begin();                 //Let's call the begin method on the server object to start the server.
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();          //To handle the actual incoming of HTTP requests, we need to call the handleClient method on the server object, on the main loop function.
+//  if(PIR_sensor.pirCheck()){
+//    Serial.println("Human detected! Shutting down...");
+//    }
+//  else{
+//    Serial.println("No human detected!");
+//    }
+    //if(PIR_sensor.pirCheck())
+}
+
+void handleRootPath() {
+  server.send(200, "text/plain", "Connected!");
+}
+
+void handleInit() {
+  steps = 0;  //Motor stopped if the arguments are wrong.
+  stepDelay = 0;
+  String message = "Initialization with: ";
+
+  if (server.hasArg("Dir")) {
+    digitalWrite(dirPin, server.arg("Dir") == "HIGH"); //This is a cunning way of checking the value of the argument Dir.
+    message += "Direction: ";
+    message += server.arg("Dir");
+  }
+  if (server.hasArg("Delay")) {
+    stepDelay = (server.arg("Delay")).toInt(); //Converts the string to integer.
+    message += " Delay: ";
+    message += server.arg("Delay");
+  }
+  if (server.hasArg("Steps")) {
+    steps = (server.arg("Steps")).toInt();
+    message += " Steps: ";
+    message += server.arg("Steps");
+  }
+
+  server.send(200, "text/plain", message); 
+  
+  for (int i = 0; i < steps; i++) { 
+    digitalWrite(stepPin, HIGH);
+    delay(stepDelay);
+    digitalWrite(stepPin, LOW);
+    delay(stepDelay);
+  }
+  Serial.println(steps); 
+}
+
+void handleRelayControl(){
+    String message = "Initialization with: ";
+    if (server.hasArg("State")) {
+    String temp = server.arg("State");
+    Serial.println(temp);
+    message += "State: ";
+    message += server.arg("State");
+  }
+  server.send(200, "text/plain", message); 
+  }
+
+void handleTimer(){
+    String message = "Initialization with: ";
+    if (server.hasArg("Duration")) {
+    String temp = server.arg("Duration");
+    Serial.println(temp);
+    message += "Duration: ";
+    message += server.arg("Duration");
+  }
+
+    if (server.hasArg("Cancel")) {
+    String temp = server.arg("Cancel");
+    Serial.println(temp);
+    message += "Cancel: ";
+    message += server.arg("Cancel");
+  }
+  server.send(200, "text/plain", message); 
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
